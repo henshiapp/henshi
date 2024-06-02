768fafce391a70275d3fb684e080d73bfd097035
neumanf <fabricionewman@protonmail.com>
2024-06-01 21:09:31 -0300

    fix: cookie parser

--- apps/users-service/src/main.ts
+++ apps/users-service/src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
-import cookieParser from 'cookie-parser';
-import { join } from 'path';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
