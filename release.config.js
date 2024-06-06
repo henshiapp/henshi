506cf9a3d393ac4cf84b7b482da659a19ce550a1
neumanf <fabricionewman@protonmail.com>
2024-06-06 19:08:29 -0300

    ci: add semantic-release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+const config = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json'],
+            },
+        ],
+        '@semantic-release/github',
+    ],
+};
+
+module.exports = config;
